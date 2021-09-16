88a2e9141b2d4a444f414d1547459f75a700c406
pRob3 <hundraprocent@me.com>
2021-09-16 20:23:54 +0200

    Added topmenu with banana count Unit and banana services

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorBattles.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,9 @@ namespace BlazorBattles.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IBananaService, BananaService>();
+            builder.Services.AddScoped<IUnitService, UnitService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IBananaService
+    {
+        event Action OnChange;
+
+        int Bananas { get; set; }
+        void EatBananas(int amount);
+
+        void AddBananas(int amount);
+    }
+}
