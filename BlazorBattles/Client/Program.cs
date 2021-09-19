4debf69f6e7871890ce11e95b69abd226b611d10
pRob3 <bubblan@slaf.se>
2021-09-19 16:19:39 +0200

    Added Blazored.LocalStorage package and Microsoft.AspNetCore.Components.Authorization Added dummy login authState

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -9,6 +9,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
 
 namespace BlazorBattles.Client
 {
@@ -19,12 +21,18 @@ namespace BlazorBattles.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddBlazoredToast();
+            builder.Services.AddBlazoredLocalStorage();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
             
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
 
             await builder.Build().RunAsync();
         }
