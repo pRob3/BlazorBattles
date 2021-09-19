367950ec2292603692638eded4cbddf712d541bd
pRob3 <bubblan@slaf.se>
2021-09-19 18:56:31 +0200

    Started on the API Added DbContext

---
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -0,0 +1,32 @@
+﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UnitController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUnits()
+        {
+            var units = await _context.Units.ToListAsync();
+
+            return Ok(units);
+        }
+    }
+}
---
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorBattles.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Unit> Units { get; set; }
+    }
+}
