7ef6ee4ab469f67dc8db2d25b373eafb0a8dd4e9
pRob3 <bubblan@slaf.se>
2021-09-23 18:33:41 +0200

    Battle model to DB Store battle history

--- BlazorBattles/Server/Data/DataContext.cs
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -1,10 +1,5 @@
 ﻿using BlazorBattles.Shared;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorBattles.Server.Data
 {
@@ -12,11 +7,29 @@ namespace BlazorBattles.Server.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Battle>()
+                .HasOne(b => b.Attacker)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Battle>()
+                .HasOne(b => b.Opponent)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+                        modelBuilder.Entity<Battle>()
+                .HasOne(b => b.Winner)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
         }
 
         public DbSet<Unit> Units { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserUnit> UserUnits { get; set; }
+        public DbSet<Battle> Battles { get; set; }
     }
 }
