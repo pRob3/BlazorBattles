e2e20276c05b56eecfd603ab5aee7361439c77fb
pRob3 <bubblan@slaf.se>
2021-09-24 06:07:01 +0200

    Added Battle Log page

--- BlazorBattles/Client/Services/IBattleService.cs
+++ BlazorBattles/Client/Services/IBattleService.cs
@@ -9,6 +9,7 @@ namespace BlazorBattles.Client.Services
 {
     public interface IBattleService
     {
+        BattleResult LastBattle { get; set; }
         Task<BattleResult> StartBattle(int opponentId);
     }
 }
